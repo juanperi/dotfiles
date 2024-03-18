7c2cb8b2bf0937382213289059951b11f6d292ec
Juan Peri <eternoperegrino@gmail.com>
2024-03-18 09:16:31 +0100

    disable diagnostics from .env files

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -7,3 +7,13 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     vim.fn.setpos(".", save_cursor)
   end,
 })
+
+-- Disable diagnostics in .env files, as every env var is reported as unused
+local group = vim.api.nvim_create_augroup("__env", { clear = true })
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = ".env",
+  group = group,
+  callback = function(args)
+    vim.diagnostic.disable(args.buf)
+  end
+})
