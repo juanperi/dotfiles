f22411b7760c543c8ab9ad30956de4778ed59a5d
Juan Peri <eternoperegrino@gmail.com>
2024-04-04 16:59:50 +0200

    open or copy to the clipboard github urls for code lines

--- home/.config/nvim/lua/plugins/fugitive.lua
+++ home/.config/nvim/lua/plugins/fugitive.lua
@@ -2,12 +2,16 @@ local M = {
   "tpope/vim-fugitive",
   lazy = false,
   keys = {
-    { "<leader>gs", ":Git<CR>" },
-    { "<leader>gc", ":Gcommit --verbose<CR>" },
-    { "<leader>gp", ":Git push<CR>" },
+    { "<leader>gs", ":Git<CR>", desc="[g]it [s]tatus" },
+    { "<leader>gc", ":Gcommit --verbose<CR>", desc="[g]it [c]ommit" },
+    { "<leader>gy", ":Git push<CR>", mode="n", desc="[g]it [y]ank Copies the github url to the clipboard" },
+    { "<leader>gy", ":GBrowse!<CR>", mode="v", desc="[g]it [y]ank Copies the github url to the clipboard" },
+    { "<leader>gb", ":GBrowse<CR>", mode="n", desc="[g]it [b]rowse Open the github url in the browser" },
+    { "<leader>gb", ":GBrowse<CR>", mode="v", desc="[g]it [b]rowse Open the github url in the browser" },
   },
   dependencies = {
     "tpope/vim-dispatch",
+    "tpope/vim-rhubarb",
   },
 }
 
