7d2679029bd9f09aaced56d57bf68b0623ac1201
Juan Peri <eternoperegrino@gmail.com>
2024-04-05 12:26:13 +0200

    add copilot chat

---
+++ home/.config/nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+      { "nvim-telescope/telescope.nvim" },
+    },
+    opts = {
+      debug = false,
+    },
+    keys = {
+      {
+        "<leader>cch",
+        function()
+          local actions = require("CopilotChat.actions")
+          require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+        end,
+        desc = "[c]opilot[c]hat - [h]elp actions",
+      },
+      -- Show prompts actions with telescope
+      {
+        "<leader>ccp",
+        function()
+          local actions = require("CopilotChat.actions")
+          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+        end,
+        desc = "[c]opilot[c]hat - [p]rompt actions",
+      },
+    }
+  },
+}
