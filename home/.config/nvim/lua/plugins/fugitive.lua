20dbb41b000798ae3e022c12abdbca04202c4df8
Juan Peri <eternoperegrino@gmail.com>
2024-04-05 12:54:19 +0200

    fix fugitive mappings

--- home/.config/nvim/lua/plugins/fugitive.lua
+++ home/.config/nvim/lua/plugins/fugitive.lua
@@ -4,7 +4,8 @@ local M = {
   keys = {
     { "<leader>gs", ":Git<CR>", desc="[g]it [s]tatus" },
     { "<leader>gc", ":Gcommit --verbose<CR>", desc="[g]it [c]ommit" },
-    { "<leader>gy", ":Git push<CR>", mode="n", desc="[g]it [y]ank Copies the github url to the clipboard" },
+    { "<leader>gp", ":Git push<CR>", mode="n", desc="[g]it [p]ush" },
+    { "<leader>gy", ":GBrowse!<CR>", mode="n", desc="[g]it [y]ank Copies the github url to the clipboard" },
     { "<leader>gy", ":GBrowse!<CR>", mode="v", desc="[g]it [y]ank Copies the github url to the clipboard" },
     { "<leader>gb", ":GBrowse<CR>", mode="n", desc="[g]it [b]rowse Open the github url in the browser" },
     { "<leader>gb", ":GBrowse<CR>", mode="v", desc="[g]it [b]rowse Open the github url in the browser" },
