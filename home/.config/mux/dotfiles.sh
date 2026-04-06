aae874990e57c05f97884d1d90c7f9342db1f3d5
Juan Peri <eternoperegrino@gmail.com>
2026-04-06 19:41:15 +0200

    use normal editor split for dotfiles

--- home/.config/mux/dotfiles.sh
+++ home/.config/mux/dotfiles.sh
@@ -5,6 +5,4 @@
 ROOT="$HOME/.homesick/repos"
 
 w=$(mux_new_session "$ROOT")
-tmux split-window -h -t "${SESSION_NAME}:${w}" -c "$ROOT"
-tmux send-keys -t "${SESSION_NAME}:${w}.1" "nvim" Enter
-tmux select-pane -t "${SESSION_NAME}:${w}.1"
+mux_split_editor "$w" "$ROOT"
