25b781446ecdedd0bfdcfab3f42d279b0c0e8df8
Juan Peri <eternoperegrino@gmail.com>
2026-04-06 11:42:59 +0200

    fixed synchronize panes functionality in tmux

--- home/.config/mux/showoff.sh
+++ home/.config/mux/showoff.sh
@@ -2,7 +2,6 @@
 w=$(mux_new_session "$HOME")
 mux_split_tiled "$w" "$HOME"
 
+tmux set-window-option -t "${SESSION_NAME}:${w}" synchronize-panes on
 tmux send-keys -t "${SESSION_NAME}:${w}.1" "clear; genact" Enter
-tmux send-keys -t "${SESSION_NAME}:${w}.2" "clear; genact" Enter
-tmux send-keys -t "${SESSION_NAME}:${w}.3" "clear; genact" Enter
-tmux send-keys -t "${SESSION_NAME}:${w}.4" "clear; genact" Enter
+tmux set-window-option -t "${SESSION_NAME}:${w}" synchronize-panes off
